f03a774a6a672dc771b475a5e7f88ae723b04159
eugene <eugene@noblesamurai.com>
2011-02-16 22:18:36 +1100

    Asserting basic functionality

--- features/basic-feature.feature
+++ features/basic-feature.feature
@@ -3,5 +3,22 @@ Feature: Run basic features
 	I want to run a simple feature
 	So that I can reduce defects in my code
 
-	Scenario:
+	Scenario: Run basic feature
 		When I run cucumis against the file "examples/addition.feature"
+		Then there should be no executable errors
+		And the "Total Errors" should be 0
+		And the "Number of Features" should be 1
+		And the "Scenario Count" should be 5
+		And the "Passed Scenario Count" should be 5
+
+		When I select the Feature "Addition"
+		Then the feature's "name" should be "Addition"
+		And the feature's "description" should be "In order to avoid silly mistakes\nAs a math idiot\nI want to be able to add up numbers\n"
+		Then the feature should have a background
+		And the feature should have 4 scenarios
+
+		When I select the Scenario "Add three numbers"
+		Then the scenario should have 7 steps
+
+		When I select the Step "Then the result should be 5 on the screen"
+		Then the step's "result" should be "pass"
---
+++ features/step_definitions/basic-feature.js
@@ -0,0 +1,163 @@
+var Steps = require('cucumis').Steps,
+    spawn = require('child_process').spawn,
+	_ = require('underscore');
+
+function runCucumis(file, cb) {
+	var ret = {
+		returnCode: 0,
+		stdOut: '',
+		stdErr: '',
+	};
+
+	var ls = spawn('cucumis', ['-f', 'json', file]);
+
+	ls.stdout.on('data', function (data) {
+		ret.stdOut = data;
+	});
+
+	ls.stderr.on('data', function (data) {
+		ret.stdErr = data;
+	});
+
+	ls.on('exit', function (code) {
+		ret.returnCode = code;
+		cb(ret);
+	});
+}
+
+var executableResults;
+var testResults;
+var currentFeature;
+var currentScenario;
+var currentStep;
+
+var testMap = {
+	'Total Errors': function() {
+		return testResults.allErrors.length;
+	},
+	'Number of Features': function() {
+		return testResults.features.length;
+	},
+	'Scenario Count': function() {
+		return testResults.stats.scenarioCount;
+	},
+	'Passed Scenario Count': function() {
+		return testResults.stats.passedScenarioCount;
+	},
+};
+
+var featureMap = {
+	'a background': function() {
+		return currentFeature.background;
+	},
+	'scenarios': function() {
+		return currentFeature.scenarios;
+	},
+	'name': function() {
+		return currentFeature.name;
+	},
+	'description': function() {
+		return currentFeature.description;
+	},
+};
+
+var scenarioMap = {
+	'steps': function() {
+		return currentScenario.steps;
+	},
+};
+
+var stepMap = {
+	'result': function() {
+		return currentStep.result;
+	},
+};
+
+Steps.When(/^I run cucumis against the file "([^"]*?)"$/, function (ctx, file) {
+	runCucumis(file, function(results) {
+		executableResults = results;
+		testResults = JSON.parse(executableResults.stdOut);
+		ctx.done();
+	});
+});
+
+Steps.Then(/^the "([^"]*?)" should be (\d+)$/, function (ctx, attr, value) {
+	testMap[attr]().should.eql(parseInt(value));
+	ctx.done();
+});
+
+Steps.Then(/^there should be no executable errors$/, function (ctx) {
+	executableResults.returnCode.should.eql(0);
+	executableResults.stdErr.should.eql('');
+	ctx.done();
+});
+
+Steps.When(/^I select the Feature "([^"]*?)"$/, function (ctx, name) {
+	var feature = testResults.features
+	                .filter(function(feature) {
+						return feature.name == name;
+	                });	
+
+	feature.length.should.eql(1);
+
+	currentFeature = feature[0];
+
+	ctx.done();
+});
+
+Steps.Then(/^the feature should have (\d+) (.*)$/, function (ctx, count, attr) {
+	featureMap[attr]().length.should.eql(parseInt(count));
+
+	ctx.done();
+});
+
+Steps.Then(/^the feature should have (.*)$/, function (ctx, attr) {
+	featureMap[attr]().should.be.ok;
+
+	ctx.done();
+});
+
+Steps.Then(/^the feature's "([^"]*?)" should be "([^"]*?)"$/, function (ctx, attr, value) {
+	featureMap[attr]().should.eql(_(value).isString() ? value.replace(/\\n/g, '\n') : value);
+	ctx.done();
+});
+
+Steps.When(/^I select the Scenario "([^"]*?)"$/, function (ctx, name) {
+	var scenario = currentFeature.scenarios
+	                .filter(function(scenario) {
+						return scenario.name == name;
+	                });	
+
+	scenario.length.should.eql(1);
+
+	currentScenario = scenario[0];
+
+	ctx.done();
+});
+
+Steps.Then(/^the scenario should have (\d+) (.*)$/, function (ctx, count, attr) {
+	scenarioMap[attr]().length.should.eql(parseInt(count));
+
+	ctx.done();
+});
+
+Steps.When(/^I select the Step "([^"]*?)"$/, function (ctx, name) {
+	var step = currentScenario.steps
+	                .filter(function(step) {
+						return step.line == name;
+	                });	
+
+	step.length.should.eql(1);
+
+	currentStep = step[0];
+
+	ctx.done();
+});
+
+
+Steps.Then(/^the step's "([^"]*?)" should be "([^"]*?)"$/, function (ctx, attr, value) {
+	stepMap[attr]().should.eql(value);
+	ctx.done();
+});
+
+Steps.export(module);
